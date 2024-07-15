01ecea2d5f3169ed2fed476a75674520027ca827
shubhneet-kaur <kaurshubhneet08@gmail.com>
2024-07-15 10:35:15 -0400

    Update Calculator.py

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,3 +1,8 @@
+def addfile(entry)
+    file = open("History.txt","w")
+    file..write(entry + "\n")
+    
+
 Shubh = True
 print("1-Addition \n2-Substraction\n3-Multiplication\n4-Division\n5-power to\n6-Rounding\n7-Remaining\noo-Exit")
 
@@ -13,24 +18,38 @@ while Shubh is True:
     opp = int(input("Enter the Operator:"))
     if opp == 1:
         Sum = First + Sec
+        Sum2 = f"{First}+{Sec}={Sum}"
+        addfile(Sum2)
         print(Sum)
     elif opp == 2:
         Sum = First - Sec
+        Sum2 = f"{First}-{Sec}={Sum}"
+        addfile(Sum2)
         print(Sum)
     elif opp == 3:
         Sum = First * Sec
+        Sum2 = f"{First}x{Sec}={Sum}"
+        addfile(Sum2)
         print(Sum)
     elif opp == 4:
         Sum = First / Sec
+        Sum2 = f"{First}/{Sec}={Sum}"
+        addfile(Sum2)
         print(Sum)
     elif opp == 5:
         Sum = First ** Sec
+        Sum2 = f"{First}^{Sec}={Sum}"
+        addfile(Sum2)
         print(Sum)
     elif opp == 6:
         Sum = First // Sec
+        Sum2 = f"{First}//{Sec}={Sum}"
+        addfile(Sum2)
         print(Sum)
     elif opp == 7:
         Sum = First % Sec
+        Sum2 = f"{First}%{Sec}={Sum}"
+        addfile(Sum2)
         print(Sum)
     elif opp == 00:
         Shubh = False
